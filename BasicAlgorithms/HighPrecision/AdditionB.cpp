a123c0adaf15883c5e3e2445687170d21f8c9faa
86159 <zxw3180716935@163.com>
2023-03-31 21:39:57 +0800

    add

---
+++ BasicAlgorithms/HighPrecision/AdditionB.cpp
@@ -0,0 +1,68 @@
+//
+// Created by 86159 on 2023-03-31.
+// 高精度加法 https://www.acwing.com/problem/content/793/
+// 圧9位代码
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int base = 1000000000;
+
+vector<int> add(vector<int> &A, vector<int> &B)
+{
+    if (A.size() < B.size()) return add(B, A);
+
+    vector<int> C;
+    int t = 0;
+    for (int i = 0; i < A.size(); i ++ )
+    {
+        t += A[i];
+        if (i < B.size()) t += B[i];
+        C.push_back(t % base);
+        t /= base;
+    }
+
+    if (t) C.push_back(t);
+    return C;
+}
+
+int main()
+{
+    string a, b;
+    vector<int> A, B;
+    cin >> a >> b;
+
+    for (int i = a.size() - 1, s = 0, j = 0, t = 1; i >= 0; i -- )
+    {
+        s += (a[i] - '0') * t;
+        j ++, t *= 10;
+        if (j == 9 || i == 0)
+        {
+            A.push_back(s);
+            s = j = 0;
+            t = 1;
+        }
+    }
+    for (int i = b.size() - 1, s = 0, j = 0, t = 1; i >= 0; i -- )
+    {
+        s += (b[i] - '0') * t;
+        j ++, t *= 10;
+        if (j == 9 || i == 0)
+        {
+            B.push_back(s);
+            s = j = 0;
+            t = 1;
+        }
+    }
+
+    auto C = add(A, B);
+
+    cout << C.back();
+    for (int i = C.size() - 2; i >= 0; i -- ) printf("%09d", C[i]);
+    cout << endl;
+
+    return 0;
+}
